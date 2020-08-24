e0f98b02482695fe9ae138cf1e00b823bcd5b595
Anthony Tulumello <anthonytulumello@gmail.com>
2020-08-24 13:59:47 -0400

    Update
    Fixed image linking, removed debug, added 1/100 chance of schrute

--- schrutify.js
+++ schrutify.js
@@ -1,20 +1,19 @@
-const path = '/schrutes/'
+const path = 'https://github.com/atulumello/schrutify/blob/master/schrutes/'
+const raw = '?raw=true'
 const schrutes = [
-    path + 'schrute_1.jpg',
-    path + 'schrute_2.jpg',
-    //path + 'schrute_3.gif',
-    'https://i.pinimg.com/originals/59/5c/61/595c616ad216778203dce4bac756adc6.jpg',
-    path + 'schrute_4.gif',
-    path + 'schrute_5.gif',
-    path + 'schrute_6.gif',
-    path + 'schrute_7.png',
-    path + 'schrute_8.jpg',
-    path + 'schrute_9.gif'
+    path + 'schrute_1.jpg' + raw,
+    path + 'schrute_2.jpg' + raw,
+    path + 'schrute_3.gif' + raw,
+    path + 'schrute_4.gif' + raw,
+    path + 'schrute_5.gif' + raw,
+    path + 'schrute_6.gif' + raw,
+    path + 'schrute_7.png' + raw,
+    path + 'schrute_8.jpg' + raw,
+    path + 'schrute_9.gif' + raw
 ]
 
 $(document).ready(function() {
     var chance = Math.floor(Math.random() * 100)
-    var chance = 69
 	if(chance == 69) {
 		schrutify()
 	}
@@ -25,7 +24,6 @@ const schrutify = () => {
         $("img").each(function(){
             var randomSchrute = schrutes[Math.floor(Math.random()*schrutes.length)]
             $(this).attr("src" , randomSchrute)
-            console.log(randomSchrute)
         })
     } , 2000)
 }
